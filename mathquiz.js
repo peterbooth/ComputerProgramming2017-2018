8575e6d413863bb2adde4b7fe5ebe5a461b203d4
peterbooth <32271469+peterbooth@users.noreply.github.com>
2017-09-25 11:26:19 -0400

    Create mathquiz.js

---
+++ mathquiz.js
@@ -0,0 +1,6 @@
+function moreStuff(){
+document.getElementById("solution").innerHTML=
+"<p> A paragraph </p> <h2>A heading</h2>" + 
+"<p>Another paragraph " +
+"with a <a href='http://www.cvuhs.org'> line</a></p>"
+}
